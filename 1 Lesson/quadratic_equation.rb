b4721336ddb08119a6eb0668d4c7eb9d52d64f28
sergo <sergo8707@gmail.com>
2016-11-08 16:02:09 +0200

    update errors

--- 1 Lesson/quadratic_equation.rb
+++ 1 Lesson/quadratic_equation.rb
@@ -1,5 +1,3 @@
-require 'mathn'
-
 puts 'Введите коэффициент a '
 a = gets.to_f
 puts 'Введите коэффициент b '
@@ -13,9 +11,10 @@ if d < 0
   puts "Дискриминант = #{d}"
   puts 'Так как дискриминант меньше нуля, то уравнение не имеет действительных корней'
 elsif d == 0
-  puts "Дискриминант = #{d}. X = #{b * -1 / 2 * a}"
+  x = b * -1 / 2 * a
+  puts "Дискриминант = #{d}. X = #{x}"
 else
-  x_one = b * -1 + Math.sqrt(d)
-  x_two = b * -1 - Math.sqrt(d)
-  puts "Дискриминант = #{d} X1 = #{x_one / 2 * a} X2 = #{x_two / 2 * a}"
+  x_one = b * -1 + Math.sqrt(d) / 2 * a
+  x_two = b * -1 - Math.sqrt(d) / 2 * a
+  puts "Дискриминант = #{d} X1 = #{x_one} X2 = #{x_two}"
 end
