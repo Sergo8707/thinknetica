544857135ba517ff66d284cc0c1311b8078750c9
Evgeniy Fateev <spaceflow@gmail.com>
2016-12-06 23:57:13 +0300

    Add lesson 6 comments

---
+++ 6_Lesson-comments/lib/trains/cargo_train.rb
@@ -0,0 +1,6 @@
+class CargoTrain < Train
+
+  def initialize(number, name, type = :cargo)
+    super
+  end
+end
---
+++ 6_Lesson-comments/lib/trains/carriages/carriage.rb
@@ -0,0 +1,27 @@
+require_relative '../company_name'
+
+class Carriage
+
+  include CompanyName
+
+  attr_reader :type
+
+  def initialize(type)
+    @type = type
+    validate!
+  end
+
+  def carriage_type
+      puts "Этот вагон #{type}"
+  end
+
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+  def validate!
+    raise 'Тип должен быть Грузовой или Пассажирский' if type != :cargo && type != :passenger
+  end
+end
